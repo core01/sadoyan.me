fd5914d69f2ebf486d77cceadff48695928c1f4d
Roman Sadoyan <sadoyan@protonmail.com>
2020-12-31 15:56:31 +0300

    update dependencies

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col max-w-3xl min-h-screen mx-auto text-black">
+    <Header />
+    <nuxt />
+  </div>
+</template>
+<script lang="ts">
+import Header from '../components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Sadoyan Roman',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Roman Sadoyan - Frontend-developer from Saint-Petersburg',
+      },
+      {
+        property: 'og:url',
+        content: 'https://sadoyan.me',
+      },
+      {
+        property: 'og:title',
+        content: 'Roman Sadoyan',
+      },
+      {
+        property: 'og:description',
+        content: 'Roman Sadoyan - Frontend-developer from Saint-Petersburg',
+      },
+      {
+        property: 'og:image',
+        content: 'https://sadoyan.me/photo.jpg',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.7.1/css/all.css',
+        integrity:
+          'sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr',
+        crossorigin: 'anonymous',
+      },
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+};
